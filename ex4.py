dd01f852e7e6cc1a99477d30b3669a3792baf228
mArcio-Lmano <marcio.mano2322@gmail.com>
2021-11-08 23:01:14 +0000

    Clean -v

--- ex4.py
+++ ex4.py
@@ -1,99 +1,22 @@
 import re
+from re_TLC import *
 
 DIR = r"processos.txt"
 
-def readline (file_dir, n_line):
-
-    with open(file_dir) as file_txt:
-        line = file_txt.readlines()
-
-    return line[n_line]
-
-def cleanName(pessoa_name):
-    
-    if pessoa_name[0] == ".":
-        expre = r"[\. ]+([A-z ]+),([A-z ]+)"
-        match = re.search(expre, pessoa_name)
-        nome = match.group(1)
-        grauPara = match.group(2)
-
-    else:
-        expre_1 = r"(([A-Z][a-z]+ ?){2,})(,([A-z ]+))?"
-        expre = r"([A-z ]+)(,([A-z]+))?"
-        match = re.search(expre_1, pessoa_name)
-        nome = match.group(1)
-        grauPara = match.group(4)
-
-    return nome, str(grauPara)
-
-def cleanPerson (pessoas_str):
-    pessoas = {}
-    pattern =  r"([A-z  \.,]+)([0-9]+)?"
-    match = re.findall(pattern, pessoas_str)
-    index = 1
-
-    for s_match in match:
-        pessoa = {}
-
-        if s_match[0] != ".":
-            nome, grau = cleanName(s_match[0])
-            pessoa["nome"] = nome
-            pessoa["grau parentesco"] = grau
-            pessoa["processo"] = s_match[1]
-            pessoas["pessoa" + str(index)] = pessoa 
-            index += 1
-
-    return pessoas
-
-def creat_jSon (info):
-
-    with open("teste.json", "w") as file_json:
-        file_json.write(str(info))
-
-    return print("Ficheiro Criado")
-
-
 def main():
     print("Exercício 4 do projeto\n")
-    line_num = 0
-    j_inf = {}
-    r = r"([0-9]+)::([0-9]{4}-[0-9]{2}-[0-9]{2})::([A-z ,\.:0-9]+)"
+    
+    json_info = {}
+    line = 1
+    while len(json_info) < 20:
+        reg = match_line(line)
+        print(reg)
+        if reg != None:
+            json_info[str(reg["idd"])] = reg
+        
+        line += 1
 
-    while len(j_inf) < 20:
-        line  = readline(DIR, line_num)
-        line_num += 1
-        match = re.search(r, line)
-        processo = {}
-
-        if match:
-            idd = match.group(1)
-            data = match.group(2)
-            pessoas = cleanPerson(match.group(3))
-            
-            while idd in j_inf:
-                print("Erro o id já existe")
-
-                if pessoas == j_inf[idd]["pessoas"]: # Falta ver datas
-                    print(f"Registo duplicado: {idd}\n")
-                    break
-
-                else:
-                    print(f"Novo registo idd mudado de {idd} para {idd}.1\n")
-                    idd = idd + ".1"
-                    processo["id"] = str(idd)
-                    processo["data"] = str(data)
-                    processo["pessoas"] = pessoas
-
-            else:
-                processo["id"] = str(idd)
-                processo["data"] = str(data)
-                processo["pessoas"] = pessoas
-
-            j_inf[str(idd)] = processo
-
-    str_json = str(j_inf)
-    str_json_inf = re.sub("\'", "\"",str_json)
-    creat_jSon (str_json_inf)
+    creat_jSon (json_info)
 
 if __name__ == "__main__":
     main()
---
+++ re_TLC.py
@@ -0,0 +1,50 @@
+import re
+
+DIR = r"processos.txt"
+
+def readline (file_dir, n_line):
+    """Função que dáda um ficheiro e um numero vai buscar a infromação
+    referente a essa linha
+
+    file_dir :: Ficheiro que pretendemos ler
+    n_linha :: Número relativo à linha que pretendemos ler
+    """
+    with open(file_dir) as file_txt:
+        line = file_txt.readlines()
+
+    return line[n_line]
+
+def cleanPersons (pessoas_str):
+    pessoas = {}
+    pattern =  r"(([A-Z][a-z ]+){2,})((,[A-z ]+). Proc.([0-9]+))?"
+    match = re.findall(pattern, pessoas_str)
+   
+    index = 1
+    for s_match in match:
+        pessoa = {}
+        pessoa["nome"] = s_match[0]
+        pessoa["grau parentesco"] = s_match[3]
+        pessoa["processo"] = s_match[4]
+        pessoas["pessoa" + str(index)] = pessoa
+        index += 1
+
+    return pessoas
+
+def creat_jSon (info):
+
+    with open("teste.json", "w") as file_json:
+        file_json.write(str(info))
+
+    return print("Ficheiro Criado")
+
+def match_line(n_linha):
+    reg_exp = r"([0-9]+)::([0-9]{4}-[0-9]{2}-[0-9]{2})::(.+)"
+    registo = {}
+    
+    match = re.search(reg_exp, readline(DIR, n_linha))
+    
+    if match:
+        registo["idd"] =  match.group(1)
+        registo["data"] = match.group(2)
+        registo["pessoas"] = cleanPersons(match.group(3))
+        return registo
