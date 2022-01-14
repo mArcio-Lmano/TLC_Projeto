21e771e3657636a74a0cd5a9da99d06bc45e2120
catarinamadaleno <pg45927@alunos.uminho.pt>
2022-01-14 18:28:48 +0000

    update tirei lexer

--- Trabalho2/yacc_teste.py
+++ Trabalho2/yacc_teste.py
@@ -1,74 +1,7 @@
-import ply.lex as lex
+from lexer import tokens
 from ply import yacc
 import sys
 
-reserved = {
-    #Tipos
-    "INT" : "INT",
-    "LIST" : "LISTA",
-    #Basic progaming shit
-    "is" : "IGUAL",
-    "IF" : "IF",
-    "THEN" : "THEN",
-    "IFNOT" : "ELSE",
-    "WHILE" : "WHILE",
-    "DO" : "DO",
-    "FOR" : "FOR",
-    #Operações mat
-    "SUM" : "PLUS",
-    "SUB" : "MINUS",
-    "MULT" : "MUL",
-    "DIV" : "DIV",
-    #Operações logicas
-    "EQ" : "EQ",
-    "BIG" : "BIG",
-    "BIGEQ" : "BIGEQ",
-    "SAMALL" : "SMALL",
-    "SAMALLEQ" : "SMALLEQ",
-    "AND" : "AND",
-    "OR" : "OR"
-    }
-
-
-tokens = ["NINT", "ID", "NEWLINE", "VIRG"] + list(reserved.values())
-
-literal = ["(", ")"]
-
-t_VIRG = r','
-
-t_NEWLINE = r';'
-
-# spaces and tabs serão ignorados
-t_ignore  =  " \t \n"
-
-# Criar floast e inteiros
-#def t_NFLOAT(t):
-#    r"\d+\.\d+"
-#    t.value = float(t.value)
-#    return t
-def t_NINT(t):
-    r"-?\d+"
-    t.value = int(t.value)
-    return t
-
-def t_ID(t):
-    r"[A-z]+"
-    t.type = reserved.get(t.value, "ID")
-    return t
-
-def t_COMMENT(t):
-    r"(\"|').*(\"|')"
-    pass
-    # No return, ignorar os comentaários
-
-def t_error(t):
-    print(f"keyword not found: {t.value[0]}")
-    t.lexer.skip(1)
-
-lexer = lex.lex()
-
-
-#parsing
 precedence = (
     ( 'left', 'PLUS', 'MINUS' ),
     ( 'left', 'MUL', 'DIV' ),
